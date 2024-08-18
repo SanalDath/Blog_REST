6e2b7232c03bdc2b483d587cd651ce9991e44929
sanal dath <149995346+SanalDath@users.noreply.github.com>
2024-08-18 14:31:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const app = express(); //init node app
+dotenv.config(); //configure dotenv
+
+app.use(bodyParser.json({ limit: '500mb' })); //bodyparser is set to parse json
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true })); //parse url encoded data
+
+
+
